7e13ac9494cc1720f5353f1570f1a2d1dafa78e7
simon <simonshuham@college.harvard.edu>
2014-04-22 22:47:04 -0400

    April 22

--- game.ml
+++ game.ml
@@ -1,28 +1,47 @@
 open Core.Std
 
-module type GAME =
-sig
+class type Game =
+object
   type move
-  type player = | Player1 | Player2
   type board
-  val initial_board : int -> int -> player -> board
-  val move_of_string : string -> move
-  (*val new_board : board -> move -> board
-  val allowed : move -> bool
-  val allowed_moves : board -> move list
-  val current_player : board -> player
-  val is_won : board -> bool*)
+  method initial_board : int -> int -> player -> board
+  method move_of_string : string -> move
+  method current_player : board -> player
+  method string_of_move : move -> string 
+  method print_board : board -> unit 
+  method allowed : move -> bool
+  method allowed_moves : board -> move list
+  method next_board : board -> move -> board 
+  method is_won : board -> bool
 end
 
-module Connect_Four =
-struct
+
+class Connect_Four player1 player2 :  =
+object
   type move = int
-  type player = | Player1 | Player2
+
   type board = (int array array) * player
-  let initial_board (player : player) : board =
-    (Array.make_matrix 6 7 0, player)
-  let move_of_string (column : string) : int =
+
+  let height = 6
+
+  let width = 7
+
+  method initial_board (player : player) : board =
+    (Array.make_matrix height width 0, player)
+
+  method move_of_string (column : string) : int =
     int_of_string column
-  let new_board (old_board : board) (new_move : move) : board =
+  
+  method string_of_move (m : move) : string = 
+    string_of_int m
+
+  method print_board (b : board) :  unit = 
+    match b with 
+    | [] -> failwith "Invalid Board" 
+    | _ -> for i = 0 to height do
+	     let bi = b.(i) in
+	     for j = 0 to width do 
+	       print_int bi.(j) 
+
     
 end
