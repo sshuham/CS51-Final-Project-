d3fa6e6ef4241466c7394e4e7f956736942b5e4f
simon <simonshuham@college.harvard.edu>
2014-04-24 11:34:16 -0400

    simon commit

--- game.ml
+++ game.ml
@@ -1,47 +1,86 @@
 open Core.Std
 
-class type Game =
+class type board =
 object
-  type move
-  type board
-  method initial_board : int -> int -> player -> board
-  method move_of_string : string -> move
-  method current_player : board -> player
-  method string_of_move : move -> string 
-  method print_board : board -> unit 
-  method allowed : move -> bool
-  method allowed_moves : board -> move list
-  method next_board : board -> move -> board 
-  method is_won : board -> bool
+  val mutable board : int array array
+  method get_height : int
+  method get_width : int
+  method get_board : int array array
 end
 
+class c4Board : board =
+object (self)
+  val mutable board = Array.make_matrix 6 7 0
+  method get_height = 6
+  method get_width = 7
+  method get_board = board
+end
 
-class Connect_Four player1 player2 :  =
+class type move =
 object
-  type move = int
+  method get_move : int
+end
 
-  type board = (int array array) * player
+class c4Move num : move =
+object
+  method get_move = num
+end
 
-  let height = 6
+class type player =
+object
+  method next_move : board -> move
+  method player_name : board -> string
+  method is_move : bool
+end
 
-  let width = 7
+class c4Player board : player = 
+object
+  method next_move board = new c4Move 0
+  method player_name board = "Bob"
+  method is_move = true
+end
 
-  method initial_board (player : player) : board =
-    (Array.make_matrix height width 0, player)
+class type game =
+object
+  method initial_board : player -> board * player
+  method move_of_string : string -> move
+  method current_player : player -> player -> player
+  method string_of_move : move -> string 
+  method print_board : board -> unit 
+  method allowed : board -> move -> bool
+  (*method next_board : board -> move -> board 
+  method is_won : board -> bool*)
+end
 
-  method move_of_string (column : string) : int =
-    int_of_string column
+class c4Game (player1 : c4Player) (player2 : c4Player) : game  =
+object (self)
+
+  val mutable board : c4Board = new c4Board
+
+  method initial_board (player : c4Player) : c4Board * c4Player =
+    (board, player1)
+
+  method move_of_string (column : string) : move =
+    new c4Move (int_of_string column)
+
+  method current_player (player1 : c4Player) (player2 : c4Player) : c4Player =
+    if player1#is_move then player1 else player2
   
-  method string_of_move (m : move) : string = 
-    string_of_int m
+  method string_of_move (m : c4Move) : string = 
+    string_of_int m#get_move
 
-  method print_board (b : board) :  unit = 
-    match b with 
-    | [] -> failwith "Invalid Board" 
-    | _ -> for i = 0 to height do
-	     let bi = b.(i) in
-	     for j = 0 to width do 
-	       print_int bi.(j) in print_newline
+  method print_board (b : c4Board) :  unit = 
+    match b#get_board with 
+    | [||] -> failwith "Invalid Board"
+    | _ -> for i = 0 to (b#get_height - 1) do
+	     let bi = b#get_board.(i) in
+	     for j = 0 to b#get_width do 
+	       (if j = b#get_width then print_newline () else print_int bi.(j)) done done
 
-    
+  method allowed (b : c4Board) (m : c4Move) : bool =
+    let move = m#get_move in
+    let board = b#get_board in
+    move < 0 || move >= b#get_width 
+    || Array.fold_right ~f: (fun x y -> phys_equal x 0) ~init:true board.(move)
+
 end
