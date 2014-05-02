bc189ebb9ebb0d1fd852bb7dc1fd382edf835c79
octavdragoi <octavdragoi@college.harvard.edu>
2014-05-02 18:08:38 -0400

    bug fixed

--- Game.ml
+++ Game.ml
@@ -193,7 +193,130 @@ let board_almost_full (b : board) =
   let vl = Array.fold_right ~f:f b ~init:0 in
   vl = 1
 
+
 class minimaxPlayer (b : board) (nump : int) : player =
+object 
+  inherit c4Player b nump 
+  val num = nump
+  
+  method! player_name = "Computer " ^ (string_of_int num)
+  method! next_move (bd : board) =
+  let rec minimax (b : board) (d : int) (num_player : int) : (int * int) = 
+    if d = 0 || board_full b || board_almost_full b 
+    then 
+      if num = num_player
+      then 
+        (let v = ref (-infty) in 
+        let index = ref 0 in 
+        for i = 0 to 6 do 
+          let mv = (i , num_player) in 
+          if allowed b mv 
+          then  
+            (let vl = estimate_value (not_alter_next_board b mv) num_player in
+            if vl > !v 
+            then (v := vl; index := i)
+            else ())
+          else ()
+        done ; (!index, !v))
+      else 
+        (let v = ref infty in 
+        let index = ref 0 in 
+        for i = 0 to 6 do 
+          let mv = (i , num_player) in 
+          if allowed b mv 
+          then  
+            (let vl = estimate_value (not_alter_next_board b mv) num_player in
+            if vl < !v 
+            then (v := vl; index := i)
+            else ())
+          else ()
+        done ; (!index, !v))
+
+
+    else
+      if num = num_player
+      then 
+        (let bestValue = ref (-infty) in
+        let indexValue = ref 0 in
+        for i = 0 to 6 do
+          let mv = (i,num_player) in
+          if allowed b mv
+          then 
+            (let (_ , vl) = minimax (not_alter_next_board b mv ) (d - 1) (3 - num_player) in 
+            if vl > !bestValue
+            then (bestValue := vl; indexValue := i)
+            else () )
+          else ()
+        done;
+        (!indexValue, !bestValue))
+      else
+        (let bestValue = ref infty in
+        let indexValue = ref 0 in
+        for i = 0 to 6 do
+          let mv = (i,num_player) in
+          if allowed b mv
+          then 
+            (let (_, vl) = minimax (not_alter_next_board b mv) (d - 1) (3 - num_player) in 
+            if vl < !bestValue
+            then (bestValue := vl; indexValue := i)
+            else () )
+          else ()
+        done;
+        (!indexValue, !bestValue))
+  in
+  let (mv, _) = minimax bd minimax_depth num in
+  (mv,num)
+  (*let rec minimax (b : board) (d : int) (num_player : int) : int = 
+    if d = 0 || estimate_value b num_player = infty || estimate_value b num_player = - infty || board_full b
+    then 
+      estimate_value b num_player 
+    else
+      if num_player = num
+      then 
+        (let bestValue = ref (-infty - 1) in
+        for i = 0 to 6 do
+          let mv = (i,num_player) in
+          if allowed b mv
+          then 
+            (let vl = minimax (not_alter_next_board b mv) (d - 1) (3 - num_player) in 
+            if vl > !bestValue
+            then (bestValue := vl)
+            else () )
+          else ()
+        done;
+         !bestValue)
+      else
+        (let bestValue = ref (infty + 1) in
+        for i = 0 to 6 do
+          let mv = (i,num_player) in
+          if allowed b mv
+          then 
+            (let vl = minimax (not_alter_next_board b mv) (d - 1) (3 - num_player) in 
+            if vl < !bestValue
+            then (bestValue := vl)
+            else () )
+          else ()
+        done;
+        !bestValue)
+  in
+
+  let bestValue = ref (-infty) in 
+  let finindexValue = ref 1 in 
+
+  for i = 0 to width - 1 do
+    if allowed b (i, num) 
+    then 
+      let vl = minimax b minimax_depth (3 - num) in
+      if !bestValue < vl
+      then (bestValue := vl ; finindexValue := i) 
+      else ()
+    else ()
+  done ; (!finindexValue, num)*)
+end  
+
+
+
+class alphabetaPlayer (b : board) (nump : int) : player =
 object 
   inherit c4Player b nump 
   val num = nump
